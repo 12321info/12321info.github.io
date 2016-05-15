847cc04eb3629fe80cdf0d05b83f12df003f03fd
12321info <12321info@gmail.com>
2016-05-16 00:04:14 +0900

    js

---
+++ a.js
@@ -0,0 +1,7 @@
+﻿    $(function(){
+     $("#bodyinfo").css({"background":"#ffffff","text-align":"center","margin":"1em 0"}).html("<span>= タップ（クリック）だけで検索（写真・詳細確認）できます =</span>");
+
+     $(".pkwtginfo").css({"background":"#ffffff","font-size":".8em","text-align":"center","margin":"-.5em 0"}).html("<span>= タップorクリックできます =</span>");
+
+     $('.adlabel').text("スポンサーリンク");
+    });//#topinfo&lt&gt
