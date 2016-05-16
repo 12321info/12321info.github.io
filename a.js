a9c21abb1159a02ad2afd7617cd7594ee278a026
12321info <12321info@gmail.com>
2016-05-16 14:22:59 +0900

    1

--- a.js
+++ a.js
@@ -4,6 +4,16 @@ elem.innerHTML = "調整中です000";
 /*var el=document.getElementsByTagName('body')[0];
 el.parentNode.insertBefore(newEl, target);*/
 
+        document.addEventListener("DOMContentLoaded",function(eve){
+            var date = new Date();
+            var year = date.getFullYear();
+            var month = date.getMonth()+1;
+            var day = date.getDate();
+
+            document.body.insertAdjacentHTML("beforeend", "<p>今日の日付は" + year + "年" + month + "月" + day + "日です。</p>");
+        },false);
+
+alert("oh1");
 
 var sp1 = document.createElement("span");
 sp1.setAttribute("id", "newSpan");
