36c33d8db3618b1777a28a8140c2715599dc4395
12321info <12321info@gmail.com>
2016-05-16 12:07:28 +0900

    1

--- a.js
+++ a.js
@@ -1,16 +1,5 @@
 ﻿function (){
-    var elem = document.getElementById("test");
-    elem.innerHTML = "あああ";
-
-document.write("<p>テスト</p>");
 alert("oh0");
-var ell=document.getElementById("to-top1");
-/*ell.innerHTML = "<p>aaaaa</p>";*/
-alert(ell);
-/*var el=document.getElementsByClassName("color1");
-el.style.transition="opacity 5s";
-el.style.opacity="0";
-alert("ex0");
-el=document.getElementByTagName("body")[0];
-el.parentNode.insertAdjacentHTML("afterbegin","<div id='editing'>調整中です</div>");*/
+    var elem = document.getElementById("test");
+    elem.innerHTML = "000";
 }
