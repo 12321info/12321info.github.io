86bd460610b289a478567664d948082823a9306e
12321info <12321info@gmail.com>
2016-05-16 16:36:00 +0900

    1

--- a.js
+++ a.js
@@ -17,9 +17,11 @@ var html="<b>BOLD</b>";
 var position="afterbegin";
 alert("oh3");
 
+function(){
 for(i=0;i<10;i++){
 document.getElementById('ul02').insertAdjacentHTML('beforeend','ffff');
 };
+}
 
 $(function(){
 var list = '';
