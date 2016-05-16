f7786e165b62b688cfe66e1ce46923535ac47bda
12321info <12321info@gmail.com>
2016-05-16 10:41:05 +0900

    1

--- a.js
+++ a.js
@@ -1,6 +1,8 @@
-﻿var el=document.getElementsByClassName('color1');
+﻿function (){
+var el=document.getElementsByClassName('color1');
 el.style.transition = 'opacity 5s';
 el.style.opacity = '0';
 el=document.getElementsByTagName('body')[0];
 
 el.parentNode.insertAdjacentHTML("afterbegin","<div id='editing'>調整中です</div>");
+}
