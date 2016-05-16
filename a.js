d68804381b50225d8286deb2cc55b563cb24850e
12321info <12321info@gmail.com>
2016-05-16 16:20:39 +0900

    1

--- a.js
+++ a.js
@@ -1,4 +1,4 @@
-﻿{alert("oh0e");
+﻿alert("oh0e");
 /*var elem = document.getElementById("test");*/
 var elem = document.querySelector('test');
 alert("oh1日");
@@ -16,14 +16,53 @@ if (navigator.userAgent.indexOf(osVer)>0){
 var html="<b>BOLD</b>";
 var position="afterbegin";
 alert("oh3");
-elem.insertAdjacentHTML(position, html);
+
+
+$(function(){
+var list = '';
+	$('button').on('click',function(){
+		var startTime = new Date();
+		list = '<li>list</li>';
+		
+		switch($(this).prop('id')){
+			case 'bt00':
+				for(i=0;i<10;i++){
+					list += '<li>list</li>';
+				};
+document.getElementById('ul00').innerHTML = list;					break;
+			case 'bt01':
+				for(i=0;i<10;i++){
+document.getElementById('ul01').innerHTML = list;					};
+				break;
+			case 'bt02':
+				var startTime = new Date();
+				for(i=0;i<10;i++){
+					document.getElementById('ul02').insertAdjacentHTML('beforeend',list);					
+				};
+				break;
+			case 'bt03':
+				for(i=0;i<10;i++){
+					$('#ul03').append(list);
+				};
+				break;				
+		};
+		var endTime = new Date();
+		console.log(endTime - startTime + "ms");
+	});
+
+})
+
+
+
+
+/*elem.insertAdjacentHTML(position, html);*/
 
 
 /*var el=document.getElementsByTagName('body')[0];
 el.parentNode.insertBefore(newEl, target);*/
 
 alert("oh4");
-
+/*
 var sp1 = document.createElement("span");
 sp1.setAttribute("id", "newSpan");
 var sp1_content = document.createTextNode("うらうら ");
@@ -31,5 +70,7 @@ sp1.appendChild(sp1_content);
 var sp2 = document.getElementById("test");
 var parentDiv = sp2.parentNode;
 // 新しい要素をDOMに sp2 の前に追加します。
-parentDiv.insertBefore(sp1, sp2);
-}
+parentDiv.insertBefore(sp1, sp2);*/
+
+
+
