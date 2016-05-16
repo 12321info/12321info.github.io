d52446843d57e2ae4402e4074add66013a082bd4
12321info <12321info@gmail.com>
2016-05-16 11:55:01 +0900

    1

--- a.js
+++ a.js
@@ -2,12 +2,12 @@
 document.write("<p>テスト</p>");
 alert("oh0");
 var ell=document.getElementById("to-top1");
-ell.innerHTML = "<p>aaaaa</p>";
+/*ell.innerHTML = "<p>aaaaa</p>";*/
 alert(ell);
-var el=document.getElementsByClassName("color1");
+/*var el=document.getElementsByClassName("color1");
 el.style.transition="opacity 5s";
 el.style.opacity="0";
 alert("ex0");
 el=document.getElementByTagName("body")[0];
-el.parentNode.insertAdjacentHTML("afterbegin","<div id='editing'>調整中です</div>");
+el.parentNode.insertAdjacentHTML("afterbegin","<div id='editing'>調整中です</div>");*/
 }
