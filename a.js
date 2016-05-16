e63e27a832cc59fb16657b67ea964cbcd587b0d4
12321info <12321info@gmail.com>
2016-05-16 12:21:24 +0900

    1

--- a.js
+++ a.js
@@ -1,3 +1,6 @@
 ﻿alert("oh0");
 var elem = document.getElementById("test");
 elem.innerHTML = "000";
+var el=document.getElementsByTagName('body')[0];
+el.parentNode.insertAdjacentHTML("afterbegin","<div id='editing'>調整中です</div>");
+
