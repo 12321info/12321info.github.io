c1b76c9b7b3367ecc4214c171cf982f40b5cbfba
12321info <12321info@gmail.com>
2016-05-16 15:13:04 +0900

    1

--- a.js
+++ a.js
@@ -4,8 +4,8 @@ alert("oh1日");
 /*elem.innerHTML="000";*/
 alert("oh2");
 
-var newtext = document.createTextNode(”てきす”);
-elem.appendChild(newtext);
+/*var newtext = document.createTextNode(”てきす”);
+elem.appendChild(newtext);*/
 
 var html = '<b>BOLD</b>';
 var position = 'afterbegin';
