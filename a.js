265f48adbcadc8c0862bb116dcabd5837dbc4213
12321info <12321info@gmail.com>
2016-05-16 15:07:09 +0900

    1

--- a.js
+++ a.js
@@ -3,6 +3,10 @@ var elem = document.getElementById("test");
 alert("oh1日");
 /*elem.innerHTML="000";*/
 alert("oh2");
+
+  var newtext = document.createTextNode(”てきす”),
+  elem.appendChild(newtext);
+
 var html = '<b>BOLD</b>';
 var position = 'afterbegin';
 elem.insertAdjacentHTML(position, html);
@@ -15,7 +19,7 @@ alert("oh3");
 
 var sp1 = document.createElement("span");
 sp1.setAttribute("id", "newSpan");
-var sp1_content = document.createTextNode("This is a new span element. ");
+var sp1_content = document.createTextNode("うらうら ");
 sp1.appendChild(sp1_content);
 var sp2 = document.getElementById("test");
 var parentDiv = sp2.parentNode;
