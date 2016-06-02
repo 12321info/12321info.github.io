7a04a78c1115b58ddf96bda1da35bd36c173fb53
12321info <12321info@users.noreply.github.com>
2016-06-02 23:51:55 +0900

    1

--- b.js
+++ b.js
@@ -1,6 +1,6 @@
 alert("oh0h");
 var o=document.getElementById("b");
-var s0="<div class='algnC title'><a class='up2em xbd c1' href=''>近未来のキーワード</a></div><form method='POST' class='algnR dn1em' name='formjs0' onsubmit='return onsub0()'><input name='txbx0' placeholder='' type='text' class='algnR ipt'/><input value='検 索' type='submit' class='sbt'/></form><script type='text/javascript'>function onsub0(){alert('ffff')\;}</script>";
+var s0="<div class='algnC title'><a class='up2em xbd c1' href=''>近未来のキーワード</a></div><form method='POST' class='algnR dn1em' name='formjs0' onsubmit='return onsub0()'><input name='txbx0' placeholder='' type='text' class='algnR ipt'/><input value='検 索' type='submit' class='sbt'/></form><script type='text/javascript'>function onsub0(){}</script>";
 
 o.insertAdjacentHTML("beforeend",s0);
 o=document.getElementById("d");
