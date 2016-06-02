081def48fea42e4d6b89c5f6dafcdf026400e798
12321info <12321info@users.noreply.github.com>
2016-06-02 15:36:05 +0900

    1

--- 500h.js
+++ 500h.js
@@ -40,13 +40,15 @@ var f=ar[1][3]-0;
 l=ar.length-1-f;
 alert(l);
 s0+="<div class='algnC dn1em lh05 btn'>":
+
+/*
 for(n=0;n<l;n++){
 s=ar[n+1][1];
 alert(n);
 s0+="<a href='https://www.google.co.jp/search?q="+(s.length<1?ar[n+1][0]:s)+"&tbm=nws'>"+ar[n+1][0]+"</a>";
 }
 
-/*
+
 if (f>0) {//代表有り
 s0+="</div><div class='algnC dn1em lh05 btn'><span class='lsp03 xbd'>代表：</span>":
 for(n=0;n<f;n++){
