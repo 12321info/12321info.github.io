7aab303bb7a4fdfe382d8e88199284bf85ac5b5e
12321info <12321info@users.noreply.github.com>
2016-06-02 15:43:22 +0900

    1

--- 500h.js
+++ 500h.js
@@ -45,11 +45,13 @@ s=ar[n+1][1];
 s0+="<a href='https://www.google.co.jp/search?q="+(s.length<1?ar[n+1][0]:s)+"&tbm=nws'>"+ar[n+1][0]+"</a>";
 }
 if (f>0) {//代表有り
+/*
 s0+="</div><div class='algnC dn1em lh05 btn'><span class='lsp03 xbd'>代表：</span>";
 for(n=0;n<f;n++){
 alert(n);
 s=ar[n+1+l][1];
 s0+="<a href='https://www.google.co.jp/search?q="+s.length<1?ar[n+1+l][0]:s+"&tbm=nws'>"+ar[n+1+l][0]+"</a>";
+*/
 }
 o.insertAdjacentHTML("beforeend",s0+"</div>";
 }
