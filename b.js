39a3759daa7bde21602da7740ae3b64af8b7bd04
12321info <12321info@users.noreply.github.com>
2016-06-02 22:39:41 +0900

    1

--- b.js
+++ b.js
@@ -1,7 +1,7 @@
 alert("oh0h");
 var o=document.getElementById("b");
 var s0="1";
-o.insertAdjacentHTML(position,s0);
+o.insertAdjacentHTML("beforeend",s0);
 o=document.getElementById("d");
 var s0="3";
-o.insertAdjacentHTML(position,s0);
+o.insertAdjacentHTML("beforeend",s0);
