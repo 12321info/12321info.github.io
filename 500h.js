0b5ec152670409386dff93aa6dc155e0e20979a1
12321info <12321info@users.noreply.github.com>
2016-06-02 17:06:17 +0900

    1

--- 500h.js
+++ 500h.js
@@ -41,7 +41,7 @@ l=ar.length-1-f;
 s0+="<div class='algnC dn1em lh25 btn'>";
 for(n=0;n<l;n++){
 s=ar[n+1][1];
-s0+="<a href='https://www.google.co.jp/search?q="+(s.length<1?ar[n+1][0]:s)+"&tbm=nws'>"+ar[n+1][0]+"</a>";
+s0+="<a href='https://www.google.co.jp/search?q="+(s.length<1?ar[n+1][0]:s)+"&tbm=nws'>"+ar[n+1][0]+"</a> ";
 }
 if (f>0) {//代表有り
 alert(f);
@@ -49,7 +49,7 @@ s0+="</div><div class='algnC dn1em lh25 btn'><span class='lsp03 xbd'>代表：</
 for(n=0;n<f;n++){
 alert(n);
 s=ar[n+1+l][1];
-s0+="<a href='https://www.google.co.jp/search?q="+(s.length<1?ar[n+1+l][0]:s)+"&tbm=nws'>"+ar[n+1+l][0]+"</a>";
+s0+="<a href='https://www.google.co.jp/search?q="+(s.length<1?ar[n+1+l][0]:s)+"&tbm=nws'>"+ar[n+1+l][0]+"</a> ";
 }
 }
 o.insertAdjacentHTML("beforeend",s0+"</div>");
