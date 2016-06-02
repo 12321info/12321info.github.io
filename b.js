5c414b34f8382ac1c731b8525d40eb04dc79ae7d
12321info <12321info@users.noreply.github.com>
2016-06-02 22:47:03 +0900

    1

--- b.js
+++ b.js
@@ -1,15 +1,6 @@
 alert("oh0h");
 var o=document.getElementById("b");
-var s0="<div class='algnC title'><a class='up2em xbd c1' href=''>近未来のキーワード</a></div>
-<form method='POST' class='algnR dn1em' name='formjs0' onsubmit='return onsub0()'>
-<input name='txbx0' placeholder='' type='text' class='algnR ipt'/><input value='検 索' type='submit' class='sbt'/>
-</form>
-<script type='text/javascript'>
-function onsub0(){
-var v=document.formjs0.txbx0.value;
-location.href='https://www.google.co.jp/search?q=site:https://how20xx.com '+v;
-return false;}
-</script>";
+var s0="<div class='algnC title'><a class='up2em xbd c1' href=''>近未来のキーワード</a></div>";
 
 o.insertAdjacentHTML("beforeend",s0);
 o=document.getElementById("d");
