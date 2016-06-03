c870105af42cf37d9227993740667a76f6025806
12321info <12321info@users.noreply.github.com>
2016-06-03 13:59:51 +0900

    1

--- 5003t.js
+++ 5003t.js
@@ -1,4 +1,4 @@
-var ar=[[0,"照明","多機能照明","情報を写しだす仕組み"],
+/*var ar=[[0,"照明","多機能照明","情報を写しだす仕組み"],
 ["テレビ","aa"],
 ["視聴端末","aa"],
 ["録画機","aa"],
@@ -10,9 +10,8 @@ var ar=[[0,"照明","多機能照明","情報を写しだす仕組み"],
 ["窓表示","aa"],
 ["（ネットワークカメラ）","aa"],
 ["（PC用モニター）","aa","5000"],
-["（次）オーディオ","aa"]];
+["（次）オーディオ","aa"]];*/
 
-/*
 var ar=[[0,"照明","多機能照明","情報を写しだす仕組み",["matome","soundc"]],
 ["テレビ","テ|レビ","","2"],
 ["視聴端末","",""],
@@ -25,14 +24,14 @@ var ar=[[0,"照明","多機能照明","情報を写しだす仕組み",["matome"
 ["窓表示","",""],
 ["（ネットワークカメラ）","",""],
 ["（PC用モニター）","",""],
-["（次）オーディオ","","gigiu"]];*/
+["（次）オーディオ","","gigiu"]];
 
 var o=document.getElementById('d');
 var r=window.location.href.split('/');
 var l=r.length;
 var s="",s0="",s1=r[l-1];
 
-if(ar[1].length>2) {//末端
+if(ar[1].length>2) {
 var f=ar[1][3]-0;
 l=ar.length-1-f;
 s0+="<div class='algnC flrx lsp01 c0'>"+ar[0][2]+"</div><div class='algnC dn2em fsr xbd c0'>"+ar[0][3]+"</div><div class='algnC dn1em lh25 btn'>";
@@ -42,7 +41,7 @@ s=ar[n+1][1];
 s0+="<a href='https://www.google.co.jp/search?q="+(s.length<1?ar[n+1][0]:s)+"&tbm=nws'>"+ar[n+1][0]+"</a> ";
 i0+=1;
 }
-if (f>0) {//代表有り
+if (f>0) {
 s0+="</div><div class='algnC dn1em lh25 btn'><span class='lsp03 xbd'>代表：</span>";
 for(n=0;n<f;n++){
 s=ar[n+1+l][1];
@@ -50,7 +49,7 @@ s0+="<a href='https://www.google.co.jp/search?q="+(s.length<1?ar[n+1+l][0]:s)+"&
 }
 }
 
-} else {//枝
+} else {
 
 s0+="<div class='algnC flrx lsp01 c0'>"+ar[0][2]+"</div><div class='algnC dn2em fsr xbd c0'>"+ar[0][3]+"</div><div class='dnward dnw2'>";
 s=s1.substring(0, s1.lastIndexOf(".html"));
