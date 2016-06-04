2863edab5b60463ca3bea79bd8e610d06010cf4e
12321info <12321info@users.noreply.github.com>
2016-06-04 17:24:57 +0900

    1

--- b.js
+++ b.js
@@ -14,6 +14,8 @@ o=document.getElementById("e");
 s0="<div class='algnC up15em dn1em c1'><div class='dnxem lh0'><a href='"+pr[c0][1]+"'>"+pr[c0][0]+"</a></div><div class='dnxem lh0'><a href='"+pr[c0][3]+"'>"+pr[c0][2]+"</a></div></div><div class='algnR dn1em upward upw2'><a href='/'>トップページ</a></div><div class='algnR fsr xbd c0'>(c)2015-2016 How20XX All Rights Reserved.</div>";
 o.insertAdjacentHTML("beforeend",s0);
 
+o.style.opacity='0';o.style.transition='opacity 5s';o.style.opacity='1';
+
 function onsub0(){
 var v=document.formjs0.txbx0.value;
 location.href="https://www.google.co.jp/search?q=site:https://how20xx.com "+v;
