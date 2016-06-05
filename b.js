5c425ed90d3bf3bd91cac00723c79bc9421a4cfa
12321info <12321info@users.noreply.github.com>
2016-06-05 20:42:50 +0900

    1

--- b.js
+++ b.js
@@ -1,13 +1,11 @@
+var ar=window.location.href.split('/');var l=ar.length;var s1=ar[l-1];
 var o=document.getElementById("c");
-var s0="<div class='algnC title'><a class='up2em xbd c1' href=''>近未来のキーワード</a></div>"+(document.body.clientWidth<500?"<br /><br /><br /><br />":"<br /><br /><br /><br /><br /><br /><br /><br /><br /><br />")+"<form method='POST' class='algnR dn1em' name='formjs0' onsubmit='return onsub0()'><input name='txbx0' placeholder='' type='text' class='algnR ipt'/><input value='検 索' type='submit' class='sbt'/></form>";
+var s0="<div class='algnC title'><a class='up2em xbd c1' href=''>近未来のキーワード</a></div>"+(document.body.clientWidth<500?"<br /><br /><br /><br />":"<br /><br /><br /><br /><br /><br /><br /><br /><br /><br />")+"<form method='POST' class='algnR dn1em' name='formjs0' onsubmit='return onsub0()'><input name='txbx0' placeholder='' type='text' class='algnR ipt'/><input value='検 索' type='submit' class='sbt'/></form><div class="AlgnL fsr">"+s1.substring(1, s1.lastIndexOf('.html'))+"</div>";
 o.insertAdjacentHTML("beforeend",s0);
 
 var pr=[["＝運営協力「０○○aaaaaaaaaaaaaaaaaaaaaaaaaaa○○○○○」様＝","https://google.com","＝最高協力「０○○○○○aaaaaaaaaaaaaaaaaaaaaaaaaaa○○」様＝","https://google.co.jp"],
 ["＝運営協力「１○○aaaaaaaaaaaaaaaaaaaaaaaaaaa○○○○○」様＝","https://google.com","＝最高協力「１○○○○○aaaaaaaaaaaaaaaaaaaaaaaaaaa○○」様＝","https://google.co.jp"],["＝運営協力「２○○aaaaaaaaaaaaaaaaaaaaaaaaaaa○○○○○」様＝","https://google.com","＝最高協力「２○○○○○aaaaaaaaaaaaaaaaaaaaaaaaaaa○○」様＝","https://google.co.jp"],["＝運営協力「３○○aaaaaaaaaaaaaaaaaaaaaaaaaaa○○○○○」様＝","https://google.com","＝最高協力「３○○○○○aaaaaaaaaaaaaaaaaaaaaaaaaaa○○」様＝","https://google.co.jp"],["＝運営協力「４○○aaaaaaaaaaaaaaaaaaaaaaaaaaa○○○○○」様＝","https://google.com","＝最高協力「４○○○○○aaaaaaaaaaaaaaaaaaaaaaaaaaa○○」様＝","https://google.co.jp"]];
 
-var ar=window.location.href.split('/');
-var l=ar.length;
-var s1=ar[l-1];
 var c0=s1.substring(1,2);
 if (c0>4) c0=4;
 o=document.getElementById("e");
