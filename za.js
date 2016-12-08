1370b1fcd5766d400ad7f5e878ffbda7f11b0018
12321info <12321info@users.noreply.github.com>
2016-12-08 12:34:32 +0900

    Update za.js

--- za.js
+++ za.js
@@ -23,6 +23,31 @@ document.getElementById('test').insertAdjacentHTML('beforeend',list);
 }
 })();
 
+/*s="「最先端見える化」計画";
+var u=window.location.href;
+var r=[
+'"http://twitter.com/share?text=" + s + "&url=" + u;',
+'"http://line.me/R/msg/text/?" + u + "%0d%0a" + s',
+'"http://www.facebook.com/sharer/sharer.php?u=" + u + "&t=" + s',
+'"http://plus.google.com/share?url=" + u',
+'"http://www.linkedin.com/shareArticle?mini=true&url=" + u + "&title=" + s',
+'"http://pinterest.com/pin/create/button/?url=" + u + "&description=" + s',
+'"http://b.hatena.ne.jp/add?mode=confirm&url=" + u + "&title=" + s'];*/
+
+/*var el = document.getElementsByClassName("socialCss");
+for(i=0;i<7;i++){
+el[i].addEventListener("click",function(){
+alert("oh12221h");
+},false);//need false!
+}*/
+
+var el = document.getElementsByClassName("socialCss")[0];
+el.style.transition = 'opacity 5s';
+el.style.opacity = '0.15';
+el.addEventListener("click",function(){
+alert("oh12221h");
+},false);//need false!
+
 alert("oh1h");
 
 /*var el=document.getElementsByTagName('body')[0];
