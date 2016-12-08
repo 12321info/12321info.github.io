90cf4281bcc0347cefbc05e939c0156b1769e781
12321info <12321info@users.noreply.github.com>
2016-12-08 14:34:02 +0900

    Update za.js

--- za.js
+++ za.js
@@ -1,3 +1,4 @@
+//work well 20161208
 
 /*var elem = document.querySelector('test');*/
 /*elem.innerHTML="000";*/
@@ -5,13 +6,11 @@
 /*var newtext = document.createTextNode(”てきす”);
 elem.appendChild(newtext);*/
 
-
-
 var elem = document.getElementById("test");
 
 var osVer = "iPhone";
 if (navigator.userAgent.indexOf(osVer)>0){
-alert(osVer);
+//alert(osVer);
 }
 
 var html="<b>BOLD</b>";
@@ -27,8 +26,7 @@ document.getElementById('test').insertAdjacentHTML('beforeend',list);
 }
 })();
 
-
-s="「最先端見える化」計画";
+s="#最先端見える化 計画";
 var u=window.location.href;
 var r=[
 "http://twitter.com/share?text=" + s + "&url=" + u,
@@ -39,17 +37,34 @@ var r=[
 "http://pinterest.com/pin/create/button/?url=" + u + "&description=" + s,
 "http://b.hatena.ne.jp/add?mode=confirm&url=" + u + "&title=" + s];
 
-var el = document.getElementsByClassName("socialCss");
+/*var el = document.getElementsByClassName("socialCss");
 for(i=1;i<7;i++){
 el[i].addEventListener("click",function(){
 alert("oh2h"+r[i]);
 window.location.href=r[i];
 },false);//need false!
-}
+}*/
 
 el[0].addEventListener("click",function(){
-alert("oh1h"+r[0]);
 window.location.href=r[0];
+},false);
+el[1].addEventListener("click",function(){
+window.location.href=r[1];
+},false);
+el[2].addEventListener("click",function(){
+window.location.href=r[2];
+},false);
+el[3].addEventListener("click",function(){
+window.location.href=r[3];
+},false);
+el[4].addEventListener("click",function(){
+window.location.href=r[4];
+},false);
+el[5].addEventListener("click",function(){
+window.location.href=r[5];
+},false);
+el[6].addEventListener("click",function(){
+window.location.href=r[6];
 },false);
 
 /*
@@ -61,7 +76,7 @@ alert("oh12221h");
 },false);//need false!
 */
 
-alert("oh5 "+r[0]);
+//alert("oh5 "+r[0]);
 
 /*var el=document.getElementsByTagName('body')[0];
 el.parentNode.insertBefore(newEl, target);*/
