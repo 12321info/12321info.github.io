cd8b04bb1a013026fe7b6487ff75b6accc65b7ff
12321info <12321info@users.noreply.github.com>
2016-12-25 09:45:42 +0900

    Update zb.js

--- zb.js
+++ zb.js
@@ -8,7 +8,7 @@ if (target.parentNode.id === 'd'){
 }, false);
 alert("any2");
 
-var ar=window.location.href.split('/');var l=ar.length;var s1=ar[l-1];
+var ar="https://topclear.net/p0.html";/*window.location.href.split('/');var l=ar.length;var s1=ar[l-1];*/
 var o=document.getElementById("c");
 var s0="<div class='algnC title'><a class='up2em lsp01 xbd c1' href='/'>#最先端見える化<!--span class='fsr lsp01 xbd c0'>近未来のキーワード</span--></a><br></div>"+(document.body.clientWidth<500?"<br /><br /><br /><br />":"<br /><br /><br /><br /><br /><br /><br /><br /><br /><br />")+"<form method='POST' class='algnR dn1em' name='formjs0' onsubmit='return onsub0()'><input name='txbx0' placeholder='' type='text' class='algnR ipt'/><input value='検 索' type='submit' class='sbt'/>　</form><div class='algnL fsr lsp03'>　"+s1.substring(1, s1.lastIndexOf('.html'))+"  </div>";
 o.insertAdjacentHTML("beforeend",s0);
