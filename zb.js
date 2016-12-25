7192eb4b68670576b9204aa030284ca60767b6d8
12321info <12321info@users.noreply.github.com>
2016-12-25 16:04:20 +0900

    Update zb.js

--- zb.js
+++ zb.js
@@ -26,7 +26,7 @@ console.log(bk+ "bk");
 
 var ar="https://topclear.net/p0.html"/*window.location.href*/.split('/');var l=ar.length;var s1=ar[l-1];
 var o=document.getElementById("c");
-var s0="<div class='algnC title'><a class='up2em lsp01 xbd c1' href='/'>#最先端見える化<!--span class='fsr lsp01 xbd c0'>近未来のキーワード</span--></a><br></div>"+(document.body.clientWidth<500?"<br /><br /><br /><br />":"<br /><br /><br /><br /><br /><br /><br /><br /><br /><br />")+"<form method='POST' class='algnR dn1em' name='formjs0' onsubmit='return onsub0()'><input name='txbx0' placeholder='' type='text' class='algnR ipt'/><input value='検 索' type='submit' class='sbt'/>　</form><div id="here" class='algnL fsr lsp03'>　"+s1.substring(1, s1.lastIndexOf('.html'))+"  </div>";
+var s0="<div class='algnC title'><a class='up2em lsp01 xbd c1' href='/'>#最先端見える化<!--span class='fsr lsp01 xbd c0'>近未来のキーワード</span--></a><br></div>"+(document.body.clientWidth<500?"<br /><br /><br /><br />":"<br /><br /><br /><br /><br /><br /><br /><br /><br /><br />")+"<form method='POST' class='algnR dn1em' name='formjs0' onsubmit='return onsub0()'><input name='txbx0' placeholder='' type='text' class='algnR ipt'/><input value='検 索' type='submit' class='sbt'/>　</form><div id='here' class='algnL fsr lsp03'>　"+s1.substring(1, s1.lastIndexOf('.html'))+"  </div>";
 o.insertAdjacentHTML("beforeend",s0);
 /*["＝運営協力「募集中 期間固定枠」様＝","https://google.com","＝最高協力「募集中 ポイント枠」様＝","https://google.co.jp"]*/
 var pr=[["＝運営協力「０○○aaaaaaaaaaaaaaaaaaaaaaaaaaa○○○○○」様＝","https://google.com","＝最高協力「０○○○○○aaaaaaaaaaaaaaaaaaaaaaaaaaa○○」様＝","https://google.co.jp"],
