7d59a10c69a2dccb037ce6d6971aaf5f48a2481a
12321info <12321info@users.noreply.github.com>
2016-12-25 13:04:06 +0900

    Update zp00.js

--- zp00.js
+++ zp00.js
@@ -1,4 +1,5 @@
-console.log("delay");alert("delay1");
+console.log("delay");
+//alert("delay1");
 var ar=[
 [0,"　上　","家（いえ）",""],["照明","Y0PO3PS082F.2X4"],["映像","ERUDE9TA71CE-"],["オーディオ","/Q2CPP36.NM9QTQ"],["家電","5UHXK7DLIPL"],["新！家電","ENXC53AO"],["家具","ULJMX/33"],["キッチン","J7527"],["水まわり","0FK1G-W-A2-78"],["子供","5RX6M"],["ペット","VW9812INT3R8.."],["植物","Z1-G133/UTR"],["小物","47U28-GMSW1"],["設備","-75R69BZ6"],["構造","FO5R7"],["街（まち）　＞","./S2W3U8","01"]
 ];
